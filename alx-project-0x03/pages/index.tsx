ce7c6add805e423ff342e21faf22712ed65a36ac
olivier01A <oliviermasengesho590@gmail.com>
2025-09-26 11:39:19 +0200

    Update index.tsx

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -1,6 +1,6 @@
 import Button from "@/components/common/Button";
 import { useRouter } from "next/router";
-import { PageRouteProps } from '@/interface';
+import { PageRouteProps } 
 interface PageRouteProps {
   pageRoute: string
 }
